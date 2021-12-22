ae623e056441f280af4e93fb416ce5f1cd117b06
Jordan <jordan.sposito2430@gmail.com>
2021-12-23 00:27:15 +1100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const discord = require('discord.js')
+var snoowrap = require('snoowrap');
+
+const { prefix } = require('./config.json');
+const { token } = require('./config.json');
+
+client.on('message', async message => {
+    
+})
+
+client.login(token)
